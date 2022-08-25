6fb397f2e63a3b36f1db1ff63dd40ff39dce1164
piyawat <piyawat>
2022-08-26 02:43:16 +0700

    no message

--- src/main.js
+++ src/main.js
@@ -4,13 +4,12 @@ import router from "./router";
 import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
-import Loading from 'vue3-loading-screen';
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
+import Loading from "vue3-loading-screen";
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
 import {
   faPenToSquare,
   faTrash,
@@ -18,13 +17,15 @@ import {
   faEnvelope,
 } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
-
-library.add(faPenToSquare, faTrash, faFacebook, faPhone,faEnvelope);
+library.add(faPenToSquare, faTrash, faFacebook, faPhone, faEnvelope);
 
 createApp(App)
   .use(store)
   .use(router)
-  .use(Loading)
+  .use(Loading, {
+    bg: "#0007",
+    slot: `<div></div>`,
+  })
   .use(VueSweetalert2)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="productList" >
+  <div class="productList">
     <h1>Product List</h1>
-    <table class="table table-hover table-striped table-bordered align-middle" v-if="this.products">
+    <table
+      class="table table-hover table-striped table-bordered align-middle"
+      v-if="this.products"
+    >
       <thead>
         <tr>
           <th scope="col" class="text-center">#</th>
@@ -21,7 +24,7 @@
             <span class="me-3" @click="editProduct(item)"
               ><font-awesome-icon icon="pen-to-square"
             /></span>
-            <span @click="deleteProduct(item._id)"
+            <span @click="checkDelete(item._id)"
               ><font-awesome-icon icon="trash"
             /></span>
           </td>
@@ -33,6 +36,7 @@
 <script>
 import axios from "axios";
 
+
 export default {
   created() {
     this.getProducts();
@@ -43,6 +47,33 @@ export default {
     };
   },
   methods: {
+    editProduct(item) {
+      console.log(item);
+
+      this.$router.push({
+        name: "formId",
+        params: {
+          id: item._id,
+        },
+      });
+    },
+    checkDelete(id) {
+      this.$swal
+        .fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!",
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            this.deleteProduct(id);
+          }
+        });
+    },
     async getProducts() {
       this.$isLoading(true);
       await axios.get("http://localhost:3000/products").then((res) => {
@@ -59,16 +90,6 @@ export default {
           this.$isLoading(false);
           this.getProducts();
         });
-    },
-    editProduct(item) {
-      console.log(item);
-
-      this.$router.push({
-        name: "formId",
-        params: {
-          id: item._id,
-        },
-      });
     },
   },
 };
