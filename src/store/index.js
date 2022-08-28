f234f1fd037ddcdcd59858585b8ea67c4cc39553
piyawat <piyawat>
2022-08-28 14:29:00 +0700

    comment store vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,32 @@
-import { createStore } from "vuex";
+// import { createStore } from "vuex";
 
-export default createStore({
-  state: {
-    products: [],
-    productForm: null,
-  },
-  getters: {
-    getProducts: (state) => state.products,
-  },
-  mutations: {
-    setProducts(state, data) {
-      state.products = data;
-    },
-    setProductForm(state, data) {
-      state.productForm = data;
-    },
-    addProduct(state, product) {
-      state.products.push(product);
-    },
-  },
-  actions: {
-    setProducts(context, value) {
-      context.commit("setProducts", value);
-    },
-    addProduct(context, value) {
-      context.commit("addProduct", value);
-    },
+// export default createStore({
+//   state: {
+//     products: [],
+//     productForm: null,
+//   },
+//   getters: {
+//     getProducts: (state) => state.products,
+//   },
+//   mutations: {
+//     setProducts(state, data) {
+//       state.products = data;
+//     },
+//     setProductForm(state, data) {
+//       state.productForm = data;
+//     },
+//     addProduct(state, product) {
+//       state.products.push(product);
+//     },
+//   },
+//   actions: {
+//     setProducts(context, value) {
+//       context.commit("setProducts", value);
+//     },
+//     addProduct(context, value) {
+//       context.commit("addProduct", value);
+//     },
 
-  },
-  modules: {},
-});
+//   },
+//   modules: {},
+// });
